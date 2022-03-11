6f6e86bc3ed2864ba1db3e262ebdbcd303207de0
Jason <jason1382311@gmail.com>
2022-03-10 21:39:58 -0800

    added table for results

--- src/Result.js
+++ src/Result.js
@@ -3,6 +3,8 @@ import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Row from 'react-bootstrap/Row'
+import Table from 'react-bootstrap/Table'
+
 
 const Result = (props) => {
     //let cow = "cow";
@@ -10,17 +12,26 @@ const Result = (props) => {
 
     return (
         <Container fluid = "md" xs = {2} lg={6}>
-            <Row>
-               <Row>
-                <Col></Col>
-                <Col >Genre</Col>
-                <Col>Actors</Col>
-                <Col>Runtime</Col>
-            </Row>
-            <Row>
-                <Col>{props.answer.title}</Col>
-            </Row> 
-            </Row>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Genre</th>
+                        <th>Director</th>
+                        <th>Runtime </th>
+                        <th>Year</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td >{props.answer.genre}</td>
+                        <td>{props.answer.director}</td>
+                        <td>{props.answer.runtime}</td>
+                        <td>{props.answer.year}</td>
+
+                    </tr>
+                </tbody>
+            </Table>
             
         </Container>
         //<h1>cow</h1>
